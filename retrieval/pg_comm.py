a17016ee0f3db7fde4f56526f894212ccc6b31d1
Seppe Van Hoof <seppe.vanhoof.1@student.kdg.be>
2025-03-22 16:32:52 +0100

    pg_comm port change

--- retrieval/pg_comm.py
+++ retrieval/pg_comm.py
@@ -9,7 +9,7 @@ db_params = {
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
     "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT") or 5432)
+    "port": int(os.getenv("DB_PORT") or 5433)
 }
 
 def get_codebase(codebase_id: str) -> {}:
